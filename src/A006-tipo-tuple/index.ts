5c85a6bd6a5217cebec4b49e49c8bd0bffa46c27
gabriel-tomas <gabriel.tomas.com@gmail.com>
2024-02-14 16:03:16 -0300

    tipo tuple

---
+++ src/A006-tipo-tuple/index.ts
@@ -0,0 +1,6 @@
+const clientData: readonly [number, string] = [2, 'Gabriel'];
+
+console.log(clientData);
+
+const array1: readonly string[] = ['Olá', 'Mundo'];
+const array2: ReadonlyArray<string> = ['Olá', 'Mundo'];
