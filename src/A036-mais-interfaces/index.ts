a0dcd0e99660c187b571ab46414162fb7bf542f6
gabriel-tomas <gabriel.tomas.com@gmail.com>
2024-03-01 16:02:31 -0300

    declaration merging

---
+++ src/A036-mais-interfaces/index.ts
@@ -0,0 +1,22 @@
+interface Pessoa {
+  nome: string;
+}
+
+interface Pessoa {
+  readonly sobrenome: string;
+}
+
+interface Pessoa {
+  readonly enderecos: readonly string[];
+}
+
+interface Pessoa {
+  readonly idade?: number;
+}
+
+const pessoa1: Pessoa = {
+  nome: 'Gabriel',
+  sobrenome: 'Tomás',
+  enderecos: ['Rua 1'],
+  idade: 31,
+};
