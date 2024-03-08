93b002cc505006f7f3b7cbf449c346e6b4080c4f
gabriel-tomas <gabriel.tomas.com@gmail.com>
2024-03-08 15:49:45 -0300

    quando o decorador de classe é chamado

---
+++ src/A052-decorator-class-2/index.ts
@@ -0,0 +1,27 @@
+@decoratorPessoa
+export class Pessoa {
+  constructor(
+    protected name: string,
+    protected surname: string,
+  ) {}
+}
+
+function decoratorPessoa<T extends new (...args: any[]) => any>(target: T) {
+  return class extends target {
+    private _completeName: string;
+    constructor(...args: any[]) {
+      super(...args);
+      this._completeName = `${this.name} ${this.surname}`;
+    }
+
+    getNomeCompleto(): string {
+      return this.completeName;
+    }
+  };
+}
+
+/* const pessoa1 = new PessoaDecorated('Gabriel', 'Tomás'); */
+const pessoa2 = new Pessoa('Gabriel', 'Tomas');
+
+/* console.log(pessoa1); */
+console.log(pessoa2);
